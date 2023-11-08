ae07202ae6296b0cc7cfe1e9d887ef3467ec109e
Sandeep Sharma <sandeepsharma@Sandeeps-MacBook-Air.local>
2023-11-08 18:36:06 +0530

    added more tests

--- cypress/e2e/api-tests/api-spec.cy.js
+++ cypress/e2e/api-tests/api-spec.cy.js
@@ -1,25 +1,31 @@
 /// <reference types="Cypress" />
 
 describe('TESTING REST API', function(){
-    it('API test Header Validation', function() {
+    it('API test for Header Validation', function() {
         cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('pokemon')
         cy.get('@pokemon').its('headers').its('content-type')
             .should('include', 'application/json; charset=utf-8')
     })
 
-    it('API test status Validation', function() {
+    it('API test for status Validation', function() {
         cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('pokemon')
         cy.get('@pokemon').its('status')
             .should('equal', 200)
     })
 
-    it('API test validate name', function() {
+    it('API test to validate name', function() {
         cy.request('https://pokeapi.co/api/v2/pokemon/25/').as('pokemon')
         cy.get('@pokemon').its('body')
             .should('include', {name: 'pikachu'})
     })
 
-    it('API test 404 status Validation', function() {
+    it('API test to validate if endpoint is accepting name', function() {
+        cy.request('https://pokeapi.co/api/v2/pokemon/pikachu/').as('pokemon')
+        cy.get('@pokemon').its('body')
+            .should('include', {name: 'pikachu'})
+    })
+
+    it('API test for 404 status Validation', function() {
         cy.request({
             method: 'GET',
             url: 'https://pokeapi.co/api/v2/pokemon/10000/',
@@ -28,5 +34,14 @@ describe('TESTING REST API', function(){
         cy.get('@pokemon').its('status')
             .should('equal', 404)
     })
-})
 
+    it('API test for bad input', function() {
+        cy.request({
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon/10,3/',
+            failOnStatusCode: false,
+        }).as('pokemon')
+        cy.get('@pokemon').its('status')
+            .should('equal', 404)
+    })
+})
